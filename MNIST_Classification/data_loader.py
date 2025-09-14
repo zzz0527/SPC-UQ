3ac27ceafd41f57056a22503037c2a19fc910632
Zhizhong Zhao <149168952+zzz0527@users.noreply.github.com>
2025-09-14 20:51:15 +0100

    Add files via upload

--- MNIST_Classification/data_loader.py
+++ MNIST_Classification/data_loader.py
@@ -6,10 +6,14 @@ import numpy as np
 from torchvision import datasets, transforms
 
 def _load_mnist():
+    current_file_path = os.path.abspath(__file__)
+    current_dir = os.path.dirname(current_file_path)
+    data_dir = os.path.join(current_dir, 'data')
+
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    train_dataset = datasets.MNIST(root='./data', train=True, download=False, transform=transform)
-    mnist_test = datasets.MNIST(root='./data', train=False, download=False, transform=transform)
-    fashion_mnist_test = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+    train_dataset = datasets.MNIST(root=data_dir, train=True, download=True, transform=transform)
+    mnist_test = datasets.MNIST(root=data_dir, train=False, download=True, transform=transform)
+    fashion_mnist_test = datasets.FashionMNIST(root=data_dir, train=False, download=True, transform=transform)
     input_shape=[28,28,1]
     num_classes=10
     return train_dataset, mnist_test, fashion_mnist_test, input_shape, num_classes
